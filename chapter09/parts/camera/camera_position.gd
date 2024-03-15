19a5e8cb58c8339fd00edb1df8200758bad723f4
Sander Vanhove <sandervhove@gmail.com>
2024-03-15 21:57:49 +0100

    Update chapter 9 according to feedback

--- chapter09/parts/camera/camera_position.gd
+++ chapter09/parts/camera/camera_position.gd
@@ -2,15 +2,14 @@ extends Node2D
 
 
 @export var camera_distance: float = 200
-@export var position_smooth_speed: float = 12.0
-@export var position_interpolation_speed: float = 55.0
+@export var position_interpolation_speed: float = 5.0
 
 
 @onready var _player: CharacterBody2D = get_parent()
 
 
-func _process(_delta):
+func _physics_process(delta: float):
 	var movement_direction: Vector2 = _player.velocity.normalized()
 	var target_position: Vector2 = movement_direction * camera_distance
-	
-	position = lerp(position, target_position, position_interpolation_speed * _delta)
+
+	position = lerp(position, target_position, position_interpolation_speed * delta)
