abc582ff7f3c4cc2cbb53ea4dbb30e331a145992
Sander Vanhove <sandervhove@gmail.com>
2024-04-04 19:04:24 +0200

    Update chapter 2

--- chapter02/variables_and_constants.gd
+++ chapter02/variables_and_constants.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 
 func _ready():
+	print("Hello, World")
+	
 	var number_of_lives = 3
 	print(number_of_lives)
-	
+
 	var fire_ball_damage = 2
-	number_of_lives -= number_of_lives
+	number_of_lives -= fire_ball_damage
 	print("The number of lives is: ", number_of_lives)
-	
+
 	const FIRE_BALL_DAMAGE = 2
 	number_of_lives -= FIRE_BALL_DAMAGE
 	print("The number of lives is: ", number_of_lives)
